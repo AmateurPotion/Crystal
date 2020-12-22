3ed1fa0715f02b7f30578328ffeeb8b6989da555
AmteurPotion <1k0j2i4@gmail.com>
2020-12-23 00:36:40 +0900

    파베 버킷 생성 코드라길래 옮겨적긴 했는데 사용법을 도통 모르겠음

---
+++ src/crystal/io/FirebaseSetup.java
@@ -0,0 +1,16 @@
+package crystal.io;
+
+
+public class FirebaseSetup {
+    public void init(String... args){
+        /* TODO: 이거 사용 방법을 찾아야됨 - 클라이언트 라이브러리 사용방법이라고 문서에 나와있긴한데 뭐라카는 건지는 모르긋다..
+            https://cloud.google.com/storage/docs/reference/libraries?authuser=0#windows
+        import com.google.cloud.storage.*;
+
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        String bucketName = args[0];
+        Bucket bucket = storage.create(BucketInfo.of(bucketName));
+         */
+
+    }
+}
