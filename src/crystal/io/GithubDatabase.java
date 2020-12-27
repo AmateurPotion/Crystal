7038047eeb5220e36c7fadffda20eec50cdf6540
AmteurPotion <1k0j2i4@gmail.com>
2020-12-27 20:12:41 +0900

    Github database... testing

--- src/crystal/io/GithubDatabase.java
+++ src/crystal/io/GithubDatabase.java
@@ -37,7 +37,6 @@ public class GithubDatabase {
     /** 모드 진입 후 네트워크 체크한 뒤에 한번만 초기화해도 충분함. 자주해서 렉걸려도 책임안짐 ㅅㄱ
      * 네트워크 체크 안 하고 이거 쓰면 게임 팅길 확률이 매우 높으니 주의! **/
     public void init(){
-        getDatabase("Database.json");
         Jval jval = getDatabase("DatabaseInfo.json");
         String version = jval.getString("version", "1");
         Jval temp = Jval.read(jval.getString("settings-version" + version));
